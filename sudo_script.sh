be820f8435c0c2bb39f1e04177579400b3e91091
Peruschi <gghgggo87@gmail.com>
2023-04-28 11:12:39 +0800

    modified sudo_script.sh

--- sudo_script.sh
+++ sudo_script.sh
@@ -8,31 +8,31 @@
 # 	sudo ./sudo_script.sh
 
 # add to dnf config file
-# echo "fastestmirror=True" >> /etc/dnf/dnf.conf
-# echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+echo "fastestmirror=True" >> /etc/dnf/dnf.conf
+echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
 
 # enable rpmfusion
-# sudo dnf install -y \
-#     	https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-# sudo dnf install -y \
-#	https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y \
+     	https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y \
+	https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
 # update system
-# dnf upgrade --refresh
-# dnf update
+dnf upgrade --refresh
+dnf update
 
 # install some software
 dnf install -y \
 	discord \
 	vim bat tree \
-	htop neofetch \
+	htop neofetch lshw\
 	timeshift
 
 # add flathub
-# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # update flatpak
-# flatpak update
+flatpak update
 
 
 # Notice
